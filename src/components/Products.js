d1fad295a5b3a5f2069e93d5627dc1d4f6978370
ARSHAN NAWAZ <arshannawaz.one@gmail.com>
2022-08-22 18:19:39 +0500

    Added Update Handler

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,30 +1,53 @@
 import React, { useState } from "react";
-import { Button, Modal, Form, Input, InputNumber } from "antd";
+import { Button, Modal, Form, Input, InputNumber,message } from "antd";
 import data from "./Data";
 const { confirm } = Modal;
 
 function Products() {
   // const [modalVisible, setmodalVisible] = useState(false);
-  const [products, setproducts] = useState(data);
+  let [products, setproducts] = useState(data);
   
   const [title, settitle] = useState("");
   const [category, setcategory] = useState("");
   const [price, setprice] = useState("");
   const [ImageURL, setImageURL] = useState("");
+  const [EditID, setEditID] = useState("");
 
+// Update Handler
+  const EditHandle = (item) =>{
+    setmodalVisible(true)
+    // settitle(item.title)
+    // setcategory(item.category)
+    // setprice(item.price)
+    // setImageURL(item.image)
+    // setEditID(item.id)
+    // console.log(item)
+    form.setFieldsValue({
+      title: item.title,
+      category: item.category,
+      price: item.price,
+      ImageURL: item.image,
+     
+    });
 
+    setEditID(item.id)
+    console.log(EditID)
+   
+  }
 
   const DeleteHandle = (id) => {
     confirm({
       title: "Do you want to delete this items?",
       okText: "Yes",
       okType: "danger",
+      cancelText: 'No',
       // icon: <ExclamationCircleOutlined />,
       // content: 'Some descriptions',
 
       onOk() {
         const deleteItem = products.filter((item) => item.id !== id);
         setproducts(deleteItem);
+        message.success('Product deleted successfully!');
       },
       onCancel() {
         console.log("Cancel");
@@ -47,25 +70,52 @@ function Products() {
     }
 
     const onFinish = () => {
-      let uid = Math.random().toString(36).substr(2, 32);
-      let newproduct = {
-        id :  uid ,
-        title: title,
-        category: category,
-        price: price,
-        image: ImageURL,
-      };
-      setproducts([newproduct, ...products]);
-      setmodalVisible(false);
-      console.log(products);
+      if (EditID) {
+        setproducts (products.map((x) => {
+            if (x.id === EditID) {
+                return {
+                    id: EditID,
+                    title: title,
+                    category: category,
+                    price: price,
+                  image: ImageURL,
+                };
+            } else {
+                return x;
+            }
+  
+        } ));
         setmodalVisible(false)
-        form.resetFields()
+          form.resetFields()
+          console.log(products);
+        }
+      
+        else {
+          let uid = Math.random().toString(36).substr(2, 32);
+          let newproduct = {
+            id :  uid ,
+            title: title,
+            category: category,
+            price: price,
+            image: ImageURL,
+          };
+          setproducts([newproduct, ...products]);
+          message.success('Product added successfully!');
+          setmodalVisible(false);
+          console.log(products);
+            setmodalVisible(false)
+            form.resetFields()
+          };
+      
     }
+    const onFinishFailed = () => {
+      message.error('Something went wrong!');
+    };
 
   return (
     <>
       <Modal 
-         title="Add a product"
+          title="Add a product"
                 visible={modalVisible}
                 onOk={handleOk}
                 onCancel={handleCancel}
@@ -73,9 +123,15 @@ function Products() {
                     <Button key="back" onClick={handleCancel}>
                         Cancel
                     </Button>,
+                     EditID ? [
                     <Button key="submit" type="primary" onClick={handleOk}>
+                        Update
+                    </Button>]:
+                   [ <Button key="submit" type="primary" onClick={handleOk}>
                         Add Product
                     </Button>,
+                ]
+                    
                 ]}
       >
         <Form form={form} 
@@ -83,11 +139,13 @@ function Products() {
             required: true,
           }}
           onFinish={onFinish}
+          onFinishFailed={onFinishFailed}
         >
           <Form.Item
             name="title"
             // write ( hasFeedback ) for validate sign
             hasFeedback
+            value={title}
             rules={[
               {
                 required: true,
@@ -99,6 +157,7 @@ function Products() {
               placeholder="Title"
               type="text"
               name="title"
+             
               onChange={(e) => {
                 settitle(e.target.value);
               }}
@@ -204,7 +263,9 @@ function Products() {
                   />
                 </td>
                 <td>
-                  <i className="bi bi-pencil-square border-0 btn-transition btn btn-outline-success " onClick={() => setmodalVisible(true)}></i>
+                  <i className="bi bi-pencil-square border-0 btn-transition btn btn-outline-success " onClick={() => {
+                    
+                  EditHandle(item)} }></i>
                   <Button
                     style={{ border: "none" }} onClick={() => DeleteHandle(item.id)}>  <i className="ms-3 bi bi-trash3 border-0 btn-transition btn btn-outline-danger"></i>
                   </Button>
